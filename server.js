32f0d6fa6a615fc32e75a9d81438869cf9178295
bassment <anton.perebyinis@gmail.com>
2016-04-06 14:41:05 +0300

    Fire-up peer server with express

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
+var PeerServer = require('peer').PeerServer;
 var express = require('express');
 var sassMiddleware = require('node-sass-middleware');
+var port = process.env.PORT || 3000;
 
-express()
-  .set('view engine', 'jade')
-  .use(
-    sassMiddleware({
-      src: __dirname + '/sass',
-      dest: __dirname + '/public/stylesheets',
-      prefix:  '/stylesheets',
-      debug: true,
-    })
- )
-  .use(express.static('./public'))
-  .get('/', function (req, res) {
-    res.render('index');
+var app = express();
+app.set('view engine', 'jade');
+app.use(
+  sassMiddleware({
+    src: __dirname + '/sass',
+    dest: __dirname + '/public/stylesheets',
+    prefix:  '/stylesheets',
+    debug: true,
   })
-  .listen(3000, function () {
-    console.log('Listening on port localhost:3000');
-  });
+);
+app.use(express.static('./public'));
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+var expressServer = app.listen(port);
+
+var io = require('socket.io').listen(expressServer);
+
+console.log('Listening on port', port);
+
+var peerServer = new PeerServer({ port: 9000, path: '/chat' });
+
+peerServer.on('connection', function (id) {
+  io.emit('user-connected', id);
+  console.log('User connected with #', id);
+});
+
+peerServer.on('disconnect', function (id) {
+  io.emit('user-disconnected', id);
+  console.log('User disconnected with #', id);
+});
