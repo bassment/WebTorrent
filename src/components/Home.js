0d4b35e7b2b39ecd2e507a5a783fc6de0e97368b
bassment <anton.perebyinis@gmail.com>
2016-04-07 16:22:01 +0300

    Add support for all formats uploads

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,33 +23,42 @@ export default class App extends React.Component {
 
   onFile = (data) => {
     const fileBytes = new Uint8Array(data.file);
-    const blob = new window.Blob([fileBytes], { type: 'audio/mp3' });
+    const blob = new window.Blob([fileBytes], { type: data.fileType });
     const urlCreator = window.URL || window.webkitURL;
     const fileUrl = urlCreator.createObjectURL(blob);
     this.setState({
       fileUrl,
-      fileName: 'New file',
+      fileName: data.fileName,
+      fileSize: data.fileSize,
     });
   };
 
   onSubmit = (e) => {
     e.preventDefault();
-    let fileInput = this.refs.fileInput.value;
-    let p2psocket = this.p2psocket;
-    if (fileInput !== '') {
+    let fileInput = this.refs.fileInput;
+    const fileName = fileInput.files[0].name;
+    const fileSize = fileInput.files[0].size;
+    const fileType = fileInput.files[0].type;
+    const p2psocket = this.p2psocket;
+    if (fileInput.value !== '') {
       let reader = new window.FileReader();
       reader.onload = function (evnt) {
-        p2psocket.emit('peer-file', { file: evnt.target.result });
+        p2psocket.emit('peer-file', {
+          file: evnt.target.result,
+          fileName,
+          fileSize,
+          fileType,
+        });
       };
 
       reader.onerror = function (err) {
         console.error('Error while reading file', err);
       };
 
-      reader.readAsArrayBuffer(this.refs.fileInput.files[0]);
+      reader.readAsArrayBuffer(fileInput.files[0]);
     }
 
-    fileInput = '';
+    fileInput.value = '';
   };
 
   render() {
@@ -59,16 +68,24 @@ export default class App extends React.Component {
         <form action="#" onSubmit={this.onSubmit}>
           <label>Select file to send</label>
           <input type="file" name="filename" ref="fileInput" size="40" />
+          <progress id="fileProgress" value="1" max="100"/>
           <br/>
           <input className="btn btn-default" type="submit" value="Submit" />
         </form>
-        <ul>
-          <li>
-            <a target="_blank" href={this.state.fileUrl}>
-              {this.state.fileName}
-            </a>
-          </li>
-        </ul>
+        {this.state.fileUrl ?
+          <ul>
+            <li>
+              <a target="_blank" href={this.state.fileUrl}>
+                {this.state.fileName}
+              </a>
+              <br/>
+              <a download href={this.state.fileUrl}>
+                Download File Above
+              </a>
+            </li>
+          </ul> :
+          null
+        }
       </div>
     );
   }
