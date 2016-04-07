fc47734a59817f8372ef0509f81cef5df673feb9
bassment <anton.perebyinis@gmail.com>
2016-04-07 13:59:17 +0300

    Setup p2p connection and add audio exchange possibility

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-var PeerServer = require('peer').PeerServer;
 var express = require('express');
+var app = express();
 var sassMiddleware = require('node-sass-middleware');
 var port = process.env.PORT || 3000;
 
-var app = express();
-app.set('view engine', 'jade');
+var server = require('http').Server(app);
+var p2pserver = require('socket.io-p2p-server').Server;
+var io = require('socket.io')(server);
+
 app.use(
   sassMiddleware({
     src: __dirname + '/sass',
@@ -14,24 +16,19 @@ app.use(
   })
 );
 app.use(express.static('./public'));
+io.use(p2pserver);
+
+app.set('view engine', 'jade');
 app.get('/', function (req, res) {
   res.render('index');
 });
 
-var expressServer = app.listen(port);
-
-var io = require('socket.io').listen(expressServer);
-
+server.listen(port);
 console.log('Listening on port', port);
 
-var peerServer = new PeerServer({ port: 9000, path: '/chat' });
-
-peerServer.on('connection', function (id) {
-  io.emit('user-connected', id);
-  console.log('User connected with #', id);
-});
-
-peerServer.on('disconnect', function (id) {
-  io.emit('user-disconnected', id);
-  console.log('User disconnected with #', id);
+io.on('connection', function (socket) {
+  socket.on('peer-file', function (data) {
+    console.log('File from peer: %s', data);
+    socket.broadcast.emit('peer-file', data);
+  });
 });
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,74 @@
+import P2P from 'socket.io-p2p';
+import io from 'socket.io-client';
+
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      fileUrl: '',
+      fileName: '',
+    };
   }
+
+  componentWillMount() {
+    this.socket = io();
+    this.opts = { peerOpts: { trickle: false }, autoUpgrade: false };
+    this.p2psocket = new P2P(this.socket, this.opts);
+    this.p2psocket.on('peer-file', this.onFile);
+  }
+
+  onFile = (data) => {
+    const fileBytes = new Uint8Array(data.file);
+    const blob = new window.Blob([fileBytes], { type: 'audio/mp3' });
+    const urlCreator = window.URL || window.webkitURL;
+    const fileUrl = urlCreator.createObjectURL(blob);
+    this.setState({
+      fileUrl,
+      fileName: 'New file',
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    let fileInput = this.refs.fileInput.value;
+    let p2psocket = this.p2psocket;
+    if (fileInput !== '') {
+      let reader = new window.FileReader();
+      reader.onload = function (evnt) {
+        p2psocket.emit('peer-file', { file: evnt.target.result });
+      };
+
+      reader.onerror = function (err) {
+        console.error('Error while reading file', err);
+      };
+
+      reader.readAsArrayBuffer(this.refs.fileInput.files[0]);
+    }
+
+    fileInput = '';
+  };
 
   render() {
     return (
       <div className="container">
-        <div className="row">
-          <h1 className="title">Hello</h1>
-        </div>
+        <h1 className="title">Hello</h1>
+        <form action="#" onSubmit={this.onSubmit}>
+          <label>Select file to send</label>
+          <input type="file" name="filename" ref="fileInput" size="40" />
+          <br/>
+          <input className="btn btn-default" type="submit" value="Submit" />
+        </form>
+        <ul>
+          <li>
+            <a target="_blank" href={this.state.fileUrl}>
+              {this.state.fileName}
+            </a>
+          </li>
+        </ul>
       </div>
     );
   }
